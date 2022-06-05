6d9699b24d27f8217cf25ea58fc1eaa8f9861a2f
Tatsuya-2 <ttjoker02@gmail.com>
2022-06-05 16:10:52 +0900

    Add: Readme and predict script.

---
+++ README.md
@@ -0,0 +1,33 @@
+<!-- GETTING STARTED -->
+## Getting Started
+### Prerequisites
+
+- Conda
+  Anaconda or Miniforge3 or ...
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/Tatsuya-2/unity_RL.git
+   ```
+2. Create environments
+   ```sh
+   cd unity_RL
+   conda env create --file myenv.yaml
+   ```
+
+<!-- USAGE EXAMPLES -->
+## Usage
+1. Run hello mlagents.
+   ```sh
+   python hello_mlagents.py
+   ```
+2. Train agents. It takes few hours.
+   ```sh
+   python train_mlagents.py
+   ```
+3. Predict agents.
+   ```sh
+   python predict_mlagents.py
+   ```
---
+++ predict_mlagents.py
@@ -0,0 +1,50 @@
+import gym
+import os
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
+from mlagents_envs.environment import UnityEnvironment
+from gym_unity.envs import UnityToGymWrapper
+
+# ログフォルダの生成
+log_dir = './logs/'
+os.makedirs(log_dir, exist_ok=True)
+
+# 環境の生成
+unity_env = UnityEnvironment(
+    file_name='3DBall', worker_id=0, no_graphics=False)
+env = UnityToGymWrapper(unity_env=unity_env, uint8_visual=True)
+env = Monitor(env, log_dir, allow_early_resets=True)
+env = DummyVecEnv([lambda: env])
+
+# モデルの生成
+model = PPO('MlpPolicy', env, verbose=1)
+
+# モデルの読み込み
+model = PPO.load('mlagents_model')
+
+# モデルの学習
+# model.learn(total_timesteps=128000)
+
+# モデルの保存
+# model.save('mlagents_model')
+
+# モデルのテスト
+state = env.reset()
+total_reward = 0
+while True:
+    # 環境の描画
+    env.render()
+
+    # 行動の取得
+    action, _ = model.predict(state)
+
+    # 1ステップ実行
+    state, reward, done, info = env.step(action)
+    total_reward += reward[0]
+
+    # エピソード完了
+    if done:
+        print('reward:', total_reward)
+        state = env.reset()
+        total_reward = 0
